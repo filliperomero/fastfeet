d01923bd89049f282f699530f15934b492cc075e
filliperomero <fillipe.romero@gmail.com>
2020-04-10 18:24:58 -0300

    chore: initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <p>FastFeet =D</p>
+    </>
+  );
+}
+
+export default App;
