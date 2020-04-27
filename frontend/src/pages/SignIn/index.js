91128dd6796a4a36460dc6f56aa472a9a6125111
filliperomero <fillipe.romero@gmail.com>
2020-04-26 21:58:38 -0300

    chore: add validation to signIn

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import UnformInput from '~/components/Form/UnformInput';
 
@@ -11,15 +12,46 @@ import logo from '~/assets/logo.png';
 export default function SignIn() {
   const dispatch = useDispatch();
   const loading = useSelector(state => state.auth.loading);
+  const formRef = useRef();
 
-  function handleSubmit({ email, password }) {
-    dispatch(signInRequest(email, password));
+  async function handleSubmit({ email, password }) {
+    formRef.current.setErrors({});
+
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .email('Insira um e-mail válido')
+          .required('E-mail obrigatório'),
+        password: Yup.string()
+          .min(6, 'Senha deve conter no mínimo 6 caracteres')
+          .required('Senha obrigatória'),
+      });
+
+      await schema.validate(
+        { email, password },
+        {
+          abortEarly: false,
+        }
+      );
+
+      dispatch(signInRequest(email, password));
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errorMessages = {};
+
+        err.inner.forEach(error => {
+          errorMessages[error.path] = error.message;
+        });
+
+        formRef.current.setErrors(errorMessages);
+      }
+    }
   }
 
   return (
     <>
       <img src={logo} alt="FastFeet" />
-      <Form onSubmit={handleSubmit}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <strong>SEU E-MAIL</strong>
         <UnformInput
           name="email"
